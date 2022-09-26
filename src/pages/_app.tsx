920fd409b09477495b7894a3a0eaecef3380409d
Augusto Marsola <augusto.marsola@live.com>
2022-09-26 18:09:52 -0300

    First global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import type { AppProps } from "next/app";
 import { globalStyle } from "../styles/global";
+import { Container, Header } from "../styles/pages/app";
+import logoImg from "../assets/logo.svg";
+import Image from "next/image";
 
 globalStyle();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
